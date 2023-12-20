b8333203aa4423f1651e3143252222ed2f6094ca
Rubí <rubi_salcedocas@outlook.es>
2023-12-20 01:49:06 -0700

    New

---
+++ src/DataType.java
@@ -0,0 +1,13 @@
+public class DataType {
+    public static void main (String[] args){
+
+        int a = 1234567890;
+        byte b = 12;
+        short c = 12345;
+        long d = 1234567891;
+        float e = 123456789;
+        double f = 123456789;
+
+        System.out.println("Nuevo");
+    }
+}
